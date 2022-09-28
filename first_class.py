d26f8e24f390f42b60c0c81090bc95c985a7b8c8
sirdesmond09 <sirdesmond09@gmail.com>
2022-09-28 14:27:49 +0100

    initial commit

---
+++ first_class.py
@@ -0,0 +1,47 @@
+# print(4)
+# print(5.4)
+# print("This is a string")
+
+# print(type("4.5"))
+
+
+#variable assignment
+
+# my_number = 40
+# my_num = 10
+
+# print(my_number/my_num)
+
+
+# + addition
+# - subtraction
+# / Normal Division
+# % Modulus Division
+# // Floor division
+# * Multiplication
+# ** Exponential
+
+# a = 4
+# b = 10
+
+
+# x = 4*b - 2*a
+
+# print(x)
+
+
+# average = (2+4+9+8+7+2)/6
+# print(average)
+
+
+a = 4
+b = 7
+c = 2
+
+numerator = (-b) + (4*a*c)**0.5
+denominator = 2*a
+
+
+x = numerator/denominator
+
+print(x)
